6e12d90b1304db73e07322f598c7ec59ab3a15c9
Nelson Chu <csnelsonchu@gmail.com>
2022-08-11 14:35:41 +0800

    setup pipeline

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    print("Active Device:{}".format(device))
+    model = pipeline('zero-shot-classification', model='facebook/bart-large-mnli', device=device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
-
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    inputs = model_inputs.get('inputs', None)
+    labels = model_inputs.get('labels', None)
+    if inputs == None:
+        return {'message': "No inputs provided"}
+    if labels == None:
+        return {'message': "No labels provided"}
     
     # Run the model
-    result = model(prompt)
+    result = model(inputs, labels)
 
     # Return the results as a dictionary
     return result
